cd3bfa76733b9beacd0e97b14124f7c2731ce594
Thorsten Lorenz <thlorenz@gmx.de>
2022-07-06 23:04:28 -0600

    chore: numerous tmux utility functions

--- lua/tmuxrun/init.lua
+++ lua/tmuxrun/init.lua
@@ -1,4 +1,15 @@
-local M = {}
+local M = {
+	vimPane = nil,
+	runnerPane = nil,
+}
+
+-- -----------------
+-- Utils
+-- -----------------
+-- Adds a pane to target with a command via the `-t` flag
+local function targetedTmuxCommand(command, targetPane)
+	return command .. " -t " .. targetPane
+end
 
 local function splitOnNewline(str)
 	local lines = {}
@@ -8,6 +19,7 @@ local function splitOnNewline(str)
 	return lines
 end
 
+-- Executes a command of form `tmux <command>`
 local function sendTmuxCommand(command)
 	local prefixedCommand = "tmux " .. command
 	local result = vim.fn.system(prefixedCommand)
@@ -15,12 +27,56 @@ local function sendTmuxCommand(command)
 	return result
 end
 
-local function tmuxPanes()
+local function focusTmuxPane(paneNumber)
+	local targetedCmd = targetedTmuxCommand("select-pane", paneNumber)
+	sendTmuxCommand(targetedCmd)
+end
+
+local function activePaneIndex()
+	return sendTmuxCommand('display-message -p "#{pane_index}"')
+end
+
+-- @returns list of tmux panes of the current window
+local function tmuxPanesCurrentWindow()
 	local panes = sendTmuxCommand("list-panes")
 	return splitOnNewline(panes)
 end
 
-local res = tmuxPanes()
-print(vim.inspect(res))
+local windowPatternRx = "^(%d+): ([-_a-zA-Z]+)[-* ]"
+
+function windowMap()
+	local windowMap = {}
+	local result = sendTmuxCommand("list-windows")
+	local lines = splitOnNewline(result)
+	for _, line in pairs(lines) do
+		print(line)
+		local n, label = line:match(windowPatternRx)
+		print(n, label)
+		if n ~= nil and label ~= nil then
+			table.insert(windowMap, n, label)
+		end
+	end
+	return windowMap
+end
+
+-- -----------------
+-- API
+-- -----------------
+
+-- Focuses the tmux pane in which this vim session started up
+function M.focusVimPane(self)
+	focusTmuxPane(self.vimPane)
+end
+
+-- Kill the tmux pane to which the tmux runner was attached
+function M.killLocalRunner()
+	-- TODO(thlorenz):
+end
+
+function M.init(self)
+	self.vimPane = activePaneIndex()
+end
+
+M:init()
 
 return M
